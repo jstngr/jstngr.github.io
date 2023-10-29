8a6f12b81f5d17e139c543caf3137de986146d1c
Tobias Justinger <justi93smail@gmail.com>
2023-10-29 19:45:05 +0100

    starting to play

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,21 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
+import FavIcon from 'assets/favicon.svg';
+
 export default function App() {
-  return <BrowserRouter>Hello World</BrowserRouter>;
+  return (
+    <>
+      <header>
+        <FavIcon />
+      </header>
+      <nav>nav</nav>
+      <aside>aside</aside>
+      <main>
+        <section>section</section>
+        <article>article</article>
+      </main>
+      <footer>footer</footer>
+    </>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import App from 'App';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import 'normalize.css';
+import './index.css';
 import './i18n';
 
-const domNode = document.getElementById('root');
+const domNode = document.getElementById('app');
 const root = createRoot(domNode);
 
 root.render(<App />);
